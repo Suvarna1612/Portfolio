973645aca77729d3a2f1ce1af84cf35c0b427911
Suvarna <134571504+Suvarna1612@users.noreply.github.com>
2025-08-17 12:35:41 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,10 +5,10 @@ export default function Contact() {
       <div style={{ margin: '2rem 0', fontSize: '1.1rem', display: 'flex', flexDirection: 'column', gap: '1.2rem' }}>
   <div><strong>Email:</strong> <a href="mailto:suvarnat1612@gmail.com" style={{ color: '#fff' }}>suvarnat1612@gmail.com</a></div>
         <div style={{ display: 'flex', gap: '1.5rem', marginTop: '1rem' }}>
-          <a href="https://github.com/SaradaSuvarnaTekumalla" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', fontWeight: 600, fontSize: '1.1rem' }}>GitHub</a>
-          <a href="https://www.linkedin.com/in/sarada-suvarna-tekumalla/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', fontWeight: 600, fontSize: '1.1rem' }}>LinkedIn</a>
-          <a href="https://leetcode.com/SaradaSuvarnaTekumalla/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', fontWeight: 600, fontSize: '1.1rem' }}>LeetCode</a>
-          <a href="https://auth.geeksforgeeks.org/user/suvarnat1612/practice/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', fontWeight: 600, fontSize: '1.1rem' }}>GeeksForGeeks</a>
+          <a href="https://github.com/Suvarna1612" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', fontWeight: 600, fontSize: '1.1rem' }}>GitHub</a>
+          <a href="https://www.linkedin.com/in/sarada-suvarna-tekumalla-107400254/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', fontWeight: 600, fontSize: '1.1rem' }}>LinkedIn</a>
+          <a href="https://leetcode.com/u/suvarna_tekumalla/" target="_blank" rel="noopener noreferrer" style={{ color: '#fff', fontWeight: 600, fontSize: '1.1rem' }}>LeetCode</a>
+         
         </div>
       </div>
     </section>
